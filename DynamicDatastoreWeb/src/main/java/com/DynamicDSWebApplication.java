9a6e8de71064eba05ff73fbbdae41b8be71b4bd5
Ahmad.Abdeen <ahmad.abdeen1@gmail.com>
2023-01-04 00:03:16 -0800

    fix pom file

--- DynamicDatastoreWeb/src/main/java/com/DynamicDSWebApplication.java
+++ DynamicDatastoreWeb/src/main/java/com/DynamicDSWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DynamicDSApplication {
+public class DynamicDSWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DynamicDSApplication.class, args);
+		SpringApplication.run(DynamicDSWebApplication.class, args);
 	}
 
 }
